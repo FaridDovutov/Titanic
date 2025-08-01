45cfcc2e5b84c557d85f00ec19900bf31c22c99a
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-08-01 22:38:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,12 @@ st.dataframe(df.sample(10), use_container_width=True)
 st.subheader("Визуализация данных")
 col1, col2 = st.columns(2)
 with col1:
-  fig1 = px.histogram(df, x="survived", color="Pclass", barmode="group", title="Выживаемость исходя от класса билета")
+  fig1 = px.histogram(df, x="Survived", color="Pclass", barmode="group", title="Выживаемость исходя от класса билета")
   st.plotly_chart(fig1, use_container_width=True)
 with col2:
-  fig2 = px.scatter(df, x="Age", y="Fare", color="survived", title="Возрасть vs Стоимость билета")
+  fig2 = px.scatter(df, x="Age", y="Fare", color="Survived", title="Возрасть vs Стоимость билета")
   st.plotly_chart(fig2, use_container_width=True)
-X = df.drop(['survived'], axis=1)
-y = df['survived']  
+X = df.drop(['Survived'], axis=1)
+y = df['Survived']  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
