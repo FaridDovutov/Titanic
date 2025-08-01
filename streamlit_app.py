a0b932bba258d14eaf40a891e1afa0a3b07ce7f2
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-08-01 23:20:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 import plotly.express as px
-from sklearn.metrics import roc_auc_score, roc_curve, auc
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score 
 
 st.set_page_config(page_title=" 🚢 Titanic Classifier", layout="wide")
 st.title(' 🚢 Titanic Classifier - Предсказание выживаемости пассажиров Титаника')
 st.write("## Работа с датасетом титаника")
 
-df = pd.read_csv('titanic_final_processed.csv')
+@st.cache_data
+def load_data():
+  df = pd.read_csv('titanic_final_processed.csv')
+   return df
+@st.cache_resource
+def train_model(df):
+    st.write("Обучение модели...")
 
 st.subheader("Случайные 10 строк")
 st.dataframe(df.sample(10), use_container_width=True)
@@ -29,4 +31,5 @@ with col2:
 X = df.drop(['Survived'], axis=1)
 y = df['Survived']  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
 
