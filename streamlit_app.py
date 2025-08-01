d7a51d3c6d20893c1648da8db7ac109cefb7ab34
FaridDovutov <barcapamir.dovutov@gmail.com>
2025-08-01 22:14:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing inmport OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score, roc_curve, auc
+from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score 
 
-st.title('🎈 App Name')
+st.set_page_config(page_title=" 🚢 Titanic Classifier", layout="wide")
+st.title(' 🚢 Titanic Classifier - Предсказание выживаемости пассажиров Титаника')
+st.write("## Работа с датасетом титаника")
 
-st.write('Hello world!')
+df = pd.read_csv('titanic_final_processed.csv')
